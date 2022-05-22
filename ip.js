ab46068f06fe86b5218eda28385343d5f0e56b69
alemprogramer <alemhhasib@gamil.com>
2022-05-22 10:40:37 +0600

    upload all file

---
+++ ip.js
@@ -0,0 +1,52 @@
+const ipURL = "https://ipapi.co/ip/";
+const axios = require("axios");
+
+const getIpAddress = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let ip = await axios.get(ipURL);
+      resolve(ip.data);
+    } catch (error) {
+      reject(error.message);
+    }
+  });
+};
+
+
+const getCountryDetails = async () => {
+  const ip = await getIpAddress();
+  return new Promise(async (resolve, reject)=> {
+    try {
+      let country = await axios.get(`https://ip-get-geolocation.com/api/json/${ip}`);
+      const ipData = country.data
+          const finalData = {
+            status: ipData.status,
+            ip: ipData.query,
+            country: ipData.country,
+            countryCode: ipData.countryCode,
+            region: ipData.region,
+            regionName: ipData.regionName,
+            city: ipData.city,
+            zip: ipData.zip,
+            lat: ipData.lat,
+            lon: ipData.lon,
+            timezone: ipData.timezone,
+            isp: ipData.isp,
+            org: ipData.org,
+            as: ipData.as,
+            mobile: ipData.mobile,
+            proxy: ipData.proxy,
+          };
+          resolve(finalData);
+    } catch
+    (error) {
+      reject(error.message);
+    }
+  });
+};
+
+
+module.exports = {
+  getCountryDetails,
+  getIpAddress,
+};
